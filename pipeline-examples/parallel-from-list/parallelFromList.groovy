149185ad21c3e6bcdce99e62b307b5d5ae6b1f35
Deivy Alejandro Reyes Morales <dareyes@sura.com.co>
2018-10-04 09:07:55 -0500

    Test 3

--- pipeline-examples/parallel-from-list/parallelFromList.groovy
+++ pipeline-examples/parallel-from-list/parallelFromList.groovy
@@ -3,7 +3,7 @@
 // parallel.
 
 // Our initial list of strings we want to echo in parallel
-def stringsToEcho = ["a", "b", "c"]
+def stringsToEcho = ["a", "b", "c", "d", "e", "f"]
 
 // The map we'll store the parallel steps in before executing them.
 def stepsForParallel = stringsToEcho.collectEntries {
